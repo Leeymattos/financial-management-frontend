927cd11d371454e20b40653ff019168f45cba261
Weslley <weslley_mattosibf@hotmail.com>
2021-06-08 16:29:02 -0300

    login page

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,46 @@
 import styles from '../styles/Home.module.css'
 
 import Link from 'next/link'
-import { FiArrowLeftCircle } from 'react-icons/fi'
+import { FiArrowRightCircle } from 'react-icons/fi'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <div>
 
-      <form>
-        <h1>Login</h1>
+        <h1>Financial Control</h1>
+        
+        <section>
+          <img src="./ilustration.svg" alt="ilustration" />
 
-        <div className={styles.inputContainer}>
-          <input id="email" type="email" pattern=".+" required />
-          <label htmlFor="email">E-mail</label>
-        </div>
+          <form>
+            <h1>Login</h1>
 
-        <div className={styles.inputContainer}>
-          <input id="password" type="password" pattern=".+" required />
-          <label htmlFor="password">Senha</label>
-        </div>
+            <div className={styles.inputContainer}>
+              <input id="email" type="email" pattern=".+" required />
+              <label htmlFor="email">E-mail</label>
+            </div>
 
-        <div>
-          <button type="submit">Entrar</button>
+            <div className={styles.inputContainer}>
+              <input id="password" type="password" pattern=".+" required />
+              <label htmlFor="password">Senha</label>
+            </div>
 
-          <Link href='/register'><a><FiArrowLeftCircle />Criar uma conta</a></Link>
+            <div>
+              <button type="submit">Entrar</button>
+            </div>
+
+            <div>
+              <Link href="/"><a><FiArrowRightCircle />Esqueci minha senha</a></Link>
+              <Link href='/register'><a><FiArrowRightCircle />Criar uma conta</a></Link>
+            </div>
+
+          </form>
+
+        </section>
 
 
-        </div>
-
-      </form>
+      </div>
 
 
     </div>
